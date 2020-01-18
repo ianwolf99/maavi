bcd494c1a38cd3382da194046c5f0e0c637edb52
Haroon Awan <mrharoonawan@Gmail.com>
2020-01-18 13:36:15 -0500

    Update maavi.rb

--- maavi.rb
+++ maavi.rb
@@ -1,30 +1,37 @@
+require 'colorize'
 require "watir"
-usage = <<TEXT
-          .         .                                                                             
-         ,8.       ,8.                   .8.                   .8. `8.`888b           ,8'  8 8888 
-        ,888.     ,888.                 .888.                 .888. `8.`888b         ,8'   8 8888 
-       .`8888.   .`8888.               :88888.               :88888. `8.`888b       ,8'    8 8888 
-      ,8.`8888. ,8.`8888.             . `88888.             . `88888. `8.`888b     ,8'     8 8888 
-     ,8'8.`8888,8^8.`8888.           .8. `88888.           .8. `88888. `8.`888b   ,8'      8 8888 
-    ,8' `8.`8888' `8.`8888.         .8`8. `88888.         .8`8. `88888. `8.`888b ,8'       8 8888 
-   ,8'   `8.`88'   `8.`8888.       .8' `8. `88888.       .8' `8. `88888. `8.`888b8'        8 8888 
-  ,8'     `8.`'     `8.`8888.     .8'   `8. `88888.     .8'   `8. `88888. `8.`888'         8 8888 
- ,8'       `8        `8.`8888.   .888888888. `88888.   .888888888. `88888. `8.`8'          8 8888 
-,8'         `         `8.`8888. .8'       `8. `88888. .8'       `8. `88888. `8.`           8 8888 
-+--- 				Fuzzing and Vulnerabilities Assessment Suite 			---+
-+--- 		    Find   Commons | OWASP | PHP | XSS | CSRF | SSRF | Template Injection	---+
-+--- 				Programmer: Haroon Awan / mrharoonawan@gmail.com 		---+
-+---						::::Usage::::					---+
-+---				  'fuzz' is a string for payload to inject  			---+
-+--- 		    Usage1: ruby maavi.rb fuzzpayloads https://www.example.com/?id=fuzz		---+
-+--- 		    Usage2: ruby maavi.rb fuzzpayloads https://www.example.com/fuzz	        ---+
-+--- 		    Usage3: ruby maavi.rb fuzzpayloads https://www.example.com/index.php/fuzz   ---+
-+--- 		    Usage4: ruby maavi.rb fuzzpayloads https://www.example.com/index.php#fuzz   ---+
-TEXT
+
+puts ""
+puts "           .         .                                                                             ".cyan
+puts "          ,8.       ,8.                   .8.                   .8. `8.`888b           ,8'  8 8888 ".cyan
+puts "         ,888.     ,888.                 .888.                 .888. `8.`888b         ,8'   8 8888 ".cyan
+puts "        .`8888.   .`8888.               :88888.               :88888. `8.`888b       ,8'    8 8888 ".cyan
+puts "       ,8.`8888. ,8.`8888.             . `88888.             . `88888. `8.`888b     ,8'     8 8888 ".cyan
+puts "      ,8'8.`8888,8^8.`8888.           .8. `88888.           .8. `88888. `8.`888b   ,8'      8 8888 ".cyan
+puts "     ,8' `8.`8888' `8.`8888.         .8`8. `88888.         .8`8. `88888. `8.`888b ,8'       8 8888 ".cyan
+puts "    ,8'   `8.`88'   `8.`8888.       .8' `8. `88888.       .8' `8. `88888. `8.`888b8'        8 8888 ".cyan
+puts "   ,8'     `8.`'     `8.`8888.     .8'   `8. `88888.     .8'   `8. `88888. `8.`888'         8 8888 ".cyan
+puts "  ,8'       `8        `8.`8888.   .888888888. `88888.   .888888888. `88888. `8.`8'          8 8888 ".cyan
+puts " .8'         `         `8.`8888. .8'       `8. `88888. .8'       `8. `88888. `8.`           8 8888 ".cyan
+puts ""
+puts "				Fuzzing and Vulnerabilities Assessment Suite			".underline
+puts "		Find Anything Anywhere. Programmer: Haroon Awan / mrharoonawan@gmail.com	".underline
+puts ""
+puts " ::::Usage::::					".cyan
+puts "[>]  Fuzz     : 'fuzz' is a string for payload to inject  			".cyan
+puts "[>]  Usage(s) : ruby maavi.rb fuzzpayloads https://www.example.com/?id=fuzz	".cyan
+puts "[>]  Usage(s) : ruby maavi.rb fuzzpayloads https://www.example.com/fuzz	        ".cyan
+puts "[>]  Usage(s) : ruby maavi.rb fuzzpayloads https://www.example.com/index.php/fuzz ".cyan
+puts "[>]  Usage(s) : ruby maavi.rb fuzzpayloads https://www.example.com/index.php#fuzz ".cyan
+
+puts ""
 if ARGV.empty?
-  puts usage
+puts "[>]  Help displayed ".cyan
+puts ""
   exit
 end
+
+puts "[>]  Starting Auto Injections ".cyan
 browser = Watir::Browser.new :firefox
 domxssfile = ARGV[0]
 victim = ARGV[1]
@@ -33,10 +40,12 @@ File.open(domxssfile, "r") do |f|
     testurl = victim.sub 'fuzz', line 
     browser.goto testurl
     sleep(2)
+	puts "[>]  Saving Assessments Injections Screenshots ".cyan
         screenshot_file = "assessment/assessment-results-#{Time.now.strftime('%Y%m%d-%H%M%S')}.png"
         browser.driver.save_screenshot(screenshot_file)        
     if browser.alert.exists?
         puts "[+] Reflection Found - Payload was : #{line}, at URL: #{testurl}"
+	puts "[>]  Saving XSS Injections Screenshots ".cyan
         screenshot_file = "xss/xss-results-#{Time.now.strftime('%Y%m%d-%H%M%S')}.png"
         browser.driver.save_screenshot(screenshot_file)        
         browser.alert.close
