26e82e0e5f62f8926d691123b9370a6e2c809539
Haroon Awan <mrharoonawan@Gmail.com>
2020-01-17 19:08:11 -0500

    Create installer.sh

---
+++ installer.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo -e "[>] Installing and Configuring";
+gem install watir
+mkdir assessment
+mkdir xss
+chmod u+x assessment/
+chmod u+x xss/
+echo -e "[>] Installed and Configured";
+echo -e "[>] run ./maavi.sh";
